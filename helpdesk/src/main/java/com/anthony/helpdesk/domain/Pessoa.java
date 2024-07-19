3abbdcbf0296b08c7bea48444a169f820c60e163
anthonyMeds <anthony.eduardo@hotmail.com>
2024-07-19 18:53:41 -0300

    feat : classe abstrata

---
+++ helpdesk/src/main/java/com/anthony/helpdesk/domain/Pessoa.java
@@ -0,0 +1,10 @@
+package com.anthony.helpdesk.domain;
+
+public abstract class Pessoa {
+
+    protected Integer id;
+    protected String nome;
+    protected String cpf;
+    protected String senha;
+
+}
