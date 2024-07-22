5e08d1d728ee58505c7e33c0514fa898edf766ab
anthonyMeds <anthony.eduardo@hotmail.com>
2024-07-22 12:15:42 -0300

    feat : data integrity validations

--- helpdesk/src/main/java/com/anthony/helpdesk/config/handlers/ResourcesExceptionHandlers.java
+++ helpdesk/src/main/java/com/anthony/helpdesk/config/handlers/ResourcesExceptionHandlers.java
@@ -1,5 +1,6 @@
 package com.anthony.helpdesk.config.handlers;
 
+import com.anthony.helpdesk.services.exceptions.DataIntegrityException;
 import com.anthony.helpdesk.services.exceptions.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,18 @@ public class ResourcesExceptionHandlers {
 
         StandardError standardError = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
                 "object not found", objectNotFoundException.getMessage(), httpServletRequest.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
+
+
+    }
+
+    @ExceptionHandler(DataIntegrityException.class)
+    public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException dataIntegrityException,
+                                                                 HttpServletRequest httpServletRequest) {
+
+        StandardError standardError = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                "data violation", dataIntegrityException.getMessage(), httpServletRequest.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
 
--- helpdesk/src/main/java/com/anthony/helpdesk/services/TecnicoService.java
+++ helpdesk/src/main/java/com/anthony/helpdesk/services/TecnicoService.java
@@ -1,13 +1,17 @@
 package com.anthony.helpdesk.services;
 
+import com.anthony.helpdesk.domain.Pessoa;
 import com.anthony.helpdesk.domain.Tecnico;
 import com.anthony.helpdesk.domain.dtos.TecnicoDTO;
+import com.anthony.helpdesk.repository.PessoaRepository;
 import com.anthony.helpdesk.repository.TecnicoRepository;
+import com.anthony.helpdesk.services.exceptions.DataIntegrityException;
 import com.anthony.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -15,6 +19,9 @@ public class TecnicoService {
 
     @Autowired
     private TecnicoRepository tecnicoRepository;
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
 
     public Tecnico findById(Integer id) {
 
@@ -32,9 +39,26 @@ public class TecnicoService {
 
     public Tecnico create(TecnicoDTO objDTO) {
 
+        validaPorCpfEEmail(objDTO);
+
         Tecnico newObj = new Tecnico(objDTO);
 
         return tecnicoRepository.save(newObj);
+
+    }
+
+    private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+
+        Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+
+        if (obj.isPresent() && !Objects.equals(obj.get().getId(), objDTO.getId())){
+            throw new DataIntegrityException("cpf já cadastrado");
+        }
+
+        obj = pessoaRepository.findByEmail(objDTO.getEmail());
+        if (obj.isPresent() && !Objects.equals(obj.get().getId(), objDTO.getId())) {
+            throw new DataIntegrityException("email já cadastrado");
+        }
 
     }
 }
