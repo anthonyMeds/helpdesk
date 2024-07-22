4ff9a025fb24a2f417791833e7cb7cf6a371bb8e
anthonyMeds <anthony.eduardo@hotmail.com>
2024-07-22 15:50:57 -0300

    feat : get chamados

--- helpdesk/src/main/java/com/anthony/helpdesk/controller/ChamadoController.java
+++ helpdesk/src/main/java/com/anthony/helpdesk/controller/ChamadoController.java
@@ -1,2 +1,28 @@
-package com.anthony.helpdesk.controller;public class ChamadoController {
+package com.anthony.helpdesk.controller;
+
+import com.anthony.helpdesk.domain.Chamado;
+import com.anthony.helpdesk.domain.dtos.ChamadoDTO;
+import com.anthony.helpdesk.services.ChamadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/chamados")
+public class ChamadoController {
+
+    @Autowired
+    private ChamadoService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
+
+        Chamado obj = service.findById(id);
+
+        return ResponseEntity.ok().body(new ChamadoDTO(obj));
+    }
+
 }
--- helpdesk/src/main/java/com/anthony/helpdesk/services/ChamadoService.java
+++ helpdesk/src/main/java/com/anthony/helpdesk/services/ChamadoService.java
@@ -1,2 +1,25 @@
-package com.anthony.helpdesk.services;public class ChamadoService {
+package com.anthony.helpdesk.services;
+
+import com.anthony.helpdesk.domain.Chamado;
+import com.anthony.helpdesk.repository.ChamadoRepository;
+import com.anthony.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findById(Integer id) {
+
+        Optional<Chamado> obj = repository.findById(id);
+
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado - id " + id));
+
+    }
+
 }
