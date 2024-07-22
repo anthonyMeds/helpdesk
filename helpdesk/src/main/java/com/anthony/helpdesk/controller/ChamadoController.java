1ac9a691fe1068efe892849e87483fe0775fd92e
anthonyMeds <anthony.eduardo@hotmail.com>
2024-07-22 15:57:43 -0300

    feat : chamado crud

--- helpdesk/src/main/java/com/anthony/helpdesk/controller/ChamadoController.java
+++ helpdesk/src/main/java/com/anthony/helpdesk/controller/ChamadoController.java
@@ -3,12 +3,21 @@ package com.anthony.helpdesk.controller;
 import com.anthony.helpdesk.domain.Chamado;
 import com.anthony.helpdesk.domain.dtos.ChamadoDTO;
 import com.anthony.helpdesk.services.ChamadoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/chamados")
@@ -23,6 +32,26 @@ public class ChamadoController {
         Chamado obj = service.findById(id);
 
         return ResponseEntity.ok().body(new ChamadoDTO(obj));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ChamadoDTO>> findAll() {
+        List<Chamado> list = service.findAll();
+        List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO obj) {
+        Chamado newObj = service.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ChamadoDTO> update(@PathVariable Integer id, @Valid @RequestBody ChamadoDTO objDTO) {
+        Chamado newObj = service.update(id, objDTO);
+        return ResponseEntity.ok().body(new ChamadoDTO(newObj));
     }
 
 }
--- helpdesk/src/main/java/com/anthony/helpdesk/services/ChamadoService.java
+++ helpdesk/src/main/java/com/anthony/helpdesk/services/ChamadoService.java
@@ -1,11 +1,26 @@
 package com.anthony.helpdesk.services;
 
 import com.anthony.helpdesk.domain.Chamado;
+import com.anthony.helpdesk.domain.Cliente;
+import com.anthony.helpdesk.domain.Tecnico;
+import com.anthony.helpdesk.domain.dtos.ChamadoDTO;
+import com.anthony.helpdesk.domain.enuns.Prioridade;
+import com.anthony.helpdesk.domain.enuns.Status;
 import com.anthony.helpdesk.repository.ChamadoRepository;
 import com.anthony.helpdesk.services.exceptions.ObjectNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,12 +29,55 @@ public class ChamadoService {
     @Autowired
     private ChamadoRepository repository;
 
+    @Autowired
+    private TecnicoService tecnicoService;
+
+    @Autowired
+    private ClienteService clienteService;
+
     public Chamado findById(Integer id) {
 
         Optional<Chamado> obj = repository.findById(id);
 
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado - id " + id));
 
+    }
+
+    public List<Chamado> findAll() {
+        return repository.findAll();
+    }
+
+    public Chamado create(ChamadoDTO obj) {
+        return repository.save(newChamado(obj));
+    }
+
+    public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+        objDTO.setId(id);
+        Chamado oldObj = findById(id);
+        oldObj = newChamado(objDTO);
+        return repository.save(oldObj);
+    }
+
+    private Chamado newChamado(ChamadoDTO obj) {
+        Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+        Cliente cliente = clienteService.findById(obj.getCliente());
+
+        Chamado chamado = new Chamado();
+        if(obj.getId() != null) {
+            chamado.setId(obj.getId());
+        }
+
+        if(obj.getStatus().equals(2)) {
+            chamado.setDataFechamento(LocalDate.now());
+        }
+
+        chamado.setTecnico(tecnico);
+        chamado.setCliente(cliente);
+        chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+        chamado.setStatus(Status.toEnum(obj.getStatus()));
+        chamado.setTitulo(obj.getTitulo());
+        chamado.setObservacoes(obj.getObservacoes());
+        return chamado;
     }
 
 }
